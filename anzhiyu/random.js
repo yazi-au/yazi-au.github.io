fc15e6909a1676c7e44224cd05c4cb69f1eabc68
yazi_au <yazi_xu@outlook.com>
2025-10-25 17:18:55 +0800

    Site updated: 2025-10-25 17:18:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/15/MyPost/"];function toRandomPost(){
+var posts=["2025/10/15/MyPost/","2025/10/25/myNewBlog/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
